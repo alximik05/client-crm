eb1e06d745ff0d0d8eb05dfe2b8fe0384ebbf77f
Stas <oalximiko@gmail.com>
2017-03-19 15:22:36 +0300

    main page hot deploy

--- client-crm/src/main/java/ru/iemz/controller/ClientController.java
+++ client-crm/src/main/java/ru/iemz/controller/ClientController.java
@@ -2,6 +2,7 @@ package ru.iemz.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -20,9 +21,12 @@ public class ClientController {
     @Autowired
     private ClientRepository clientRepository;
 
-    @ResponseBody
+
     @GetMapping
-    public List<Client> getAllClients() {
-        return clientRepository.findAll();
+    public String getAllClients(Model model) {
+        model.addAttribute("clients", clientRepository.findAll());
+        return "clientsList";
     }
+
+
 }
--- client-crm/src/main/java/ru/iemz/controller/MainController.java
+++ client-crm/src/main/java/ru/iemz/controller/MainController.java
@@ -1,6 +1,7 @@
 package ru.iemz.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -9,12 +10,12 @@ import org.springframework.web.bind.annotation.ResponseBody;
  * Created by stas on 09/03/17.
  */
 @Controller
-@RequestMapping("/")
 public class MainController {
 
-    @ResponseBody
     @GetMapping
-    public String hello() {
-        return "Hello, братан";
+    @RequestMapping("/")
+    public String index() {
+        return "index";
     }
+
 }
